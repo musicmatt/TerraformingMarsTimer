a72297c46e0faad9d89da6b8d65b1e873d47a59b
Sui15 <suiryu15@gmail.com>
2020-09-06 02:59:02 +0200

    Edited setup

--- TerraformingMarsTimer.ino
+++ TerraformingMarsTimer.ino
@@ -5,9 +5,10 @@
 //-------------------------------- DATA TYPES ---------------------------------
 
 //---------------------- PRIVATE FUNCTION PROTOTYPES --------------------------
-
+void print_out_structs (int num_players, PLAYER_T *players);
 //------------------------- STATIC DATA & CONSTANTS ---------------------------
-
+static int ite = 0;
+static int cd_minutes = 0;
 //------------------------------- GLOBAL DATA ---------------------------------
 int num_players = 1;
 //------------------------------ PUBLIC FUNCTIONS -----------------------------
@@ -24,11 +25,27 @@ void setup() {
   while(keypad.getKey() != '0')
   {
   } 
-  while (phase_setup() != 1);  // koliko igraca, raspored/boja, vrijeme po igracu
   
+  num_players = select_num_players(); // koliko igraca
+  
+  PLAYER_T *players = malloc (num_players * sizeof(PLAYER_T));
+  
+  for (ite = 0; ite < num_players; ite++)
+  {
+    players[ite].color = pick_color(ite);    // raspored boja
+  }
+
+  cd_minutes = countdown_timer_setup();
+  Serial.print("minutes: ");
+  Serial.println(cd_minutes);
+
+  start_screen(num_players, cd_minutes, players);
+
+  print_out_structs(num_players, players);
 }
 
 void loop() {
+
   #if 0
   next_player();
   next_generation();
@@ -36,5 +53,15 @@ void loop() {
 }
 
 
-
+void print_out_structs (int num_players, PLAYER_T *players)
+{
+  for (ite = 0; ite < num_players; ite++)
+  {
+    Serial.print("player.color");
+    Serial.print(players[ite].color);
+    Serial.print("\t player.cd_time");
+    Serial.println(players[ite].cd_time);
+    
+  } 
+}
 //---------------------------- INTERRUPT HANDLERS -----------------------------
