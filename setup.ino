80d3a1f35b6885a20419e6933c0aaad1c5de3510
Sui15 <suiryu15@gmail.com>
2020-09-06 03:02:19 +0200

    Implemented global functions for setting up the timer
    Functions are for selecting the number of players, choosing a color for
    each player (NOT IDIOT PROOF), selecting the countdown time, and the
    start screen

--- setup.ino
+++ setup.ino
@@ -6,102 +6,156 @@
 //-------------------------------- DATA TYPES ---------------------------------
 
 //---------------------- PRIVATE FUNCTION PROTOTYPES --------------------------
-void pick_color (PLAYER_T);
 //------------------------- STATIC DATA & CONSTANTS ---------------------------
-enum states{
-  NUM_PLAYERS =         0U,
-  PLAYER_COLOR =        1U,
-  COUNTDOWN_TIME =      2U
-};
-
-static int state = NUM_PLAYERS;
 static char key = 0;
 static char perma_key = 0;
-static int ite = 0;
 
 //------------------------------- GLOBAL DATA ---------------------------------
 
 //------------------------------ PUBLIC FUNCTIONS -----------------------------
-int phase_setup(void)
-{
-  switch (state)
-  {
-    case NUM_PLAYERS:
-      lcd.clear();
-      lcd.print("Num players:");
-      lcd.setCursor(0, 1);
-      lcd.noCursor();
-      while(1)
-      {
-        while((key = keypad.getKey()) == NO_KEY);
-#ifndef DEBUG
-        Serial.println(key);
-#endif
-        if (key == '#')
-        {
-          break;
-        }
-        if ((MIN_PLAYERS <= key) && (MAX_PLAYERS >= key))
-        {
-          perma_key = key;
-          lcd.print("                ");
-          lcd.setCursor(0, 1);
-          lcd.print(perma_key);
-#ifndef DEBUG
-          Serial.print("--------    ");
-          Serial.println(perma_key);
-#endif
-        }
-      }
-      
-      num_players = perma_key - 48;
-#ifndef DEBUG
-      Serial.print("num_players: ");
-      Serial.println(num_players);
-#endif
-
-      PLAYER_T *players = malloc (num_players * sizeof(PLAYER_T));
-      
-#ifndef DEBUG
-      Serial.println("1");
-#endif
-
-      state = PLAYER_COLOR;
-      break;
-      
-    case PLAYER_COLOR:
-
-#ifndef DEBUG
-      Serial.println("2");
-#endif
-
-      for (ite = 0; ite < num_players; ite++)
-      {
-#ifndef DEBUG
-      Serial.println("3");
-#endif
-        pick_color(players[ite]);
-      }
-    
-      break;
-      
-    case COUNTDOWN_TIME:
-      return 1;
-      break;
-      
-    default:
-      break;
-  }
+void phase_setup(void)
+{      
+  // not implemented
 }
 
+int select_num_players (void)
+{
+  lcd.clear();
+  lcd.print("Num players:");
+  lcd.setCursor(0, 1);
+  lcd.noCursor();
+  while(1)
+  {
+    while((key = keypad.getKey()) == NO_KEY);
+#ifndef DEBUG
+    Serial.println(key);
+#endif
+    if (key == '#')
+    {
+      break;
+    }
+    if ((MIN_PLAYERS <= key) && (MAX_PLAYERS >= key))
+    {
+      perma_key = key;
+      lcd.print("                ");
+      lcd.setCursor(0, 1);
+      lcd.print(perma_key);
+#ifndef DEBUG
+      Serial.print("--------    ");
+      Serial.println(perma_key);
+#endif
+    }
+  }
+  
+  return perma_key - 48;
+}
 
-//---------------------------- PRIVATE FUNCTIONS ------------------------------
-void pick_color (PLAYER_T)
+char* pick_color (int ite)
 {
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("player ");
-  lcd.print(ite);
-  while(1);
+  lcd.print(ite+1);
+  lcd.print(" color:");
+  while(1)
+  {
+    while((key = keypad.getKey()) == NO_KEY);
+#ifndef DEBUG
+    Serial.println(key);
+#endif
+    if (key == '#')
+    {
+      break;
+    }
+    if ((MIN_PLAYERS <= key) && (MAX_PLAYERS >= key))
+    {
+      perma_key = key - 48;
+      lcd.setCursor(0, 1);
+      lcd.print("                ");
+      lcd.setCursor(0, 1);
+      lcd.print(colors[perma_key-1]);
+#ifndef DEBUG
+      Serial.print("--------    ");
+      Serial.println(colors[perma_key-1]);
+#endif
+    }  
+  }
+  return colors[perma_key-1];
 }
+
+int countdown_timer_setup (void)
+{
+  static int setc = 0;
+  int minutes = 0;
+  String mins;
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Minutes on timer");
+  lcd.setCursor(0,1);
+  while(1)
+  {
+    while((key = keypad.getKey()) == NO_KEY);
+#ifndef DEBUG
+    Serial.println(key);
+#endif
+    if (key == '*')   // reset query
+    {
+      mins = "";
+      setc = 0;
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Minutes on timer");
+      lcd.setCursor(0,1);
+    }
+    
+    if (key == '#') // done with entering minutes
+    {
+      break;
+    }
+    
+    if (('0' <= key) && ('9' >= key))
+    {
+      lcd.print(key);
+      setc++;
+      lcd.setCursor(setc, 1);
+      mins = mins + key;
+      
+#ifndef DEBUG
+      Serial.print("--------    ");
+      Serial.println(key);
+#endif
+    }  
+  }
+  minutes = mins.toInt();
+  return minutes;
+}
+
+void start_screen (int num_players, int minutes, PLAYER_T *players)
+{
+  lcd.clear();
+  lcd.print("Start?");
+  lcd.setCursor(0,1);
+  lcd.print(num_players);
+  lcd.print("P      ");
+  lcd.print(minutes);
+  lcd.print("min");
+  
+  while(1)
+  {
+    while((key = keypad.getKey()) == NO_KEY);
+#ifndef DEBUG
+    Serial.println(key);
+#endif
+    if (key == '#') // done with entering minutes
+    {
+      break;
+    }
+  }
+
+  for (ite = 0; ite < num_players; ite++)
+  {
+    players[ite].cd_time = minutes * 60;
+  }
+}
+//---------------------------- PRIVATE FUNCTIONS ------------------------------
 //---------------------------- INTERRUPT HANDLERS -----------------------------
